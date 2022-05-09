1c7652c8eecee80dd564377d8d819aea0bc317a1
Orhan Mert Erduran <100988268+Coopernickuus@users.noreply.github.com>
2022-05-09 14:48:45 +0300

    Add files via upload
    Menü geçişleri ve arayüzleri

---
+++ GameTitleScreen/MenuController.java
@@ -0,0 +1,110 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.Interpolator;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class MenuController implements Initializable {
+
+    @FXML
+    private ImageView snakeTitleImage, mouseTitleImage;
+
+    ImageView pause;
+    @FXML
+    private AnchorPane myAnchorPane;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    protected Button gotoMainMenu;
+        Button pauseButton;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        ScaleTransition scale1 = new ScaleTransition();
+        ScaleTransition scale2 = new ScaleTransition();
+        scale1.setNode(snakeTitleImage);
+        scale1.setDuration(Duration.millis(3000));
+        scale1.setCycleCount(TranslateTransition.INDEFINITE);
+        scale1.setInterpolator(Interpolator.LINEAR);
+        scale1.setByX(1.0);
+        scale1.setByY(1.0);
+        scale1.setAutoReverse(true);
+        scale1.play();
+        scale2.setNode(mouseTitleImage);
+        scale2.setDuration(Duration.millis(3000));
+        scale2.setCycleCount(TranslateTransition.INDEFINITE);
+        scale2.setInterpolator(Interpolator.LINEAR);
+        scale2.setByX(1.0);
+        scale2.setByY(1.0);
+        scale2.setAutoReverse(true);
+        scale2.play();
+    }
+
+    public void logout(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("Do you really want to log out");
+        alert.setTitle("Logging out");
+        alert.setContentText("Please click yes if you want to log out.");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) myAnchorPane.getScene().getWindow();
+            System.out.println("Logged out");
+            stage.close();
+        }
+    }
+
+    public void switchToGameModeMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("GameModeMenu.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("resources/application.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToMainMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("resources/MainMenu.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("resources/application.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToGameZone(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Gamezone.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("resources/application.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToPauseMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("PauseMenu.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("resources/application.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
